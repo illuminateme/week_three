8767de6183e0d41830422411b77708ad7c4471a0
illuminateme <aimaatigari@gmail.com>
2021-06-21 20:42:29 +0100

    First commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    for (i in 1..100) {
+        when {
+            i % 15 == 0 -> println("FizzBuzz")
+            i % 5 == 0 -> {
+                println("Buzz")
+            }
+            i % 3 == 0 -> {
+                println("Fizz")
+            }
+            else -> {
+                println(i)
+            }
+        }
+    }
+}
